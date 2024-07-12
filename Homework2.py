99410e27993203c9a1dd36f500f742a28c76379b
tmeALF <m1160955@yandex.ru>
2024-07-12 14:20:09 +0300

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,5 @@
+first_name = "Курс: Python,"
+total_tasks = 12
+hours_spent = 1.5
+Average_execution_time = hours_spent / total_tasks
+print (first_name, 'всего задач:',total_tasks,',', 'затрачено часов:',hours_spent,',', 'среднее время выполнения', Average_execution_time, 'часа.')
