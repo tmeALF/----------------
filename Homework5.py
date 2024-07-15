dae3f8d3094cdc78b1d1d847f94c835038758eea
tmeALF <m1160955@yandex.ru>
2024-07-15 14:31:44 +0300

    Add files via upload

---
+++ Homework5.py
@@ -0,0 +1,16 @@
+immutable_var = 1, 2, "banana", True
+print(immutable_var)
+#immutable_var.remove(2) - данная команда не сработает, потому что
+#в переменной применен кортеж с неизеняемыми данными.
+#Для того, чтобы можно было изменить, нужно применить список.
+#Например:
+#immutable_var = [1, 2, "banana"]
+#print(immutable_var)
+#immutable_var.remove(2)
+#print(immutable_var)
+mutable_list = [1, 2, "banana", True]
+mutable_list[0] = 1 * 2
+mutable_list[2] = '2+2=4'
+mutable_list[3] = (mutable_list[0] + mutable_list[1] == 4)
+mutable_list.append(":)")
+print(mutable_list)
